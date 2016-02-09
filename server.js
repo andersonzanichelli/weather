25657ebcec28114a0e911dfe671281e53d5d284f
Anderson Zanichelli <anderzanic@gmail.com>
2016-02-09 17:35:28 -0200

    modularização do weather

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var restify = require('restify');
 var mongodb = require('mongodb');
+var randomicWeather = require('./randomicWeather.js');
 
 var server = restify.createServer();
 var uri = 'mongodb://weather-provider:weather-provider@ds057954.mongolab.com:57954/weather'
@@ -10,7 +11,7 @@ server.use(restify.bodyParser({ mapParams: true }));
 var weather = {};
 
 weather.provider = function() {
-	return "";
+	return "First Weather Provider";
 };
 
 weather.hello = function(req, res, next) {
@@ -19,11 +20,40 @@ weather.hello = function(req, res, next) {
 };
 
 weather.now = function(req, res, next) {
-	var token = req.params['token'];
-	//descriptografar o token json
-	res.json(randomicWeather());
+    var user = {
+        "email": req.body['email'],
+        "password": req.body['password']
+    };
 
-	next();
+    console.log(user);
+
+    var params = {
+        "callback": weather.find,
+        "collection": 'users',
+        "filter": user,
+        "response": res
+    };
+
+    console.log('before operations');
+
+    weather.dbOperations(params);
+    next();
+};
+
+weather.find = function(params) {
+    var collection = params.db.collection(params.collection);
+    collection.find(params.filter).toArray(function(err, docs) {
+        if(err) {
+            console.log('Error...');
+            params.response.json(err);
+            return;
+        }
+
+        if(docs.length > 0)
+        	params.response.json(randomicWeather.now(weather.provider()));
+
+        return;
+    });
 };
 
 weather.register = function(req, res, next) {
@@ -45,56 +75,28 @@ weather.register = function(req, res, next) {
 };
 
 weather.dbOperations = function(params) {
+	console.log('operations');
     mongodb.MongoClient.connect(uri, function(err, db) {
-        if(err) throw err;
+
+        if(err){
+        	console.log('error' + err);
+        	throw err;
+        }
 
         params.db = db;
-        params.callback(params);
+
+        if(params.callback)
+        	params.callback(params);
     });
 };
 
 server.get('/hello/:param', weather.hello);
 server.get('/register/:email/:password', weather.register);
-server.get('/weather/:token', weather.now);
+server.post('/weather', weather.now);
 
 server.listen(port, function() {
   console.log('%s listening at server port %s', 'Weather Info', port);
 });
-
-var randomicWeather = function() {
-
-	var sky = function(temperature) {
-		var situation = ["weather-snowy", "weather-rainy", "weather-stormy", "weather-cloudy", "weather-cloudy-sunny", "weather-sunny"];
-		if(temperature > 0 && (temperature % 6) == 0)
-			return situation[temperature / 6]
-	
-		return temperature > 0 ? situation[temperature % 6] : situation[0];
-	}
-
-	var city = function() {
-		var cities = ["Curitiba", "Maringá", "Londrina", "Foz do Iguaçu", "Guarapuava"];
-		var chosen = Math.floor((Math.random() * 5) + 1);
-
-		return cities[chosen - 1];
-	}
-
-	var positive = Math.floor((Math.random() * 36) + 1);
-	var negative = Math.floor((Math.random() * 6) + 1);
-	var humidity = Math.floor((Math.random() * 100) + 1);
-
-	var temperature = positive - negative;
-	
-	var info = {
-		"provider": weather.provider(),
-		"city": city(),
-		"temperature": temperature,
-		"Humidity": humidity,
-		"sky": sky(temperature),
-		"update": new Date()
-	};
-
-	return info;
-};
 
 var saveUser = function(params) {
 	var collection = params.db.collection(params.collection);
